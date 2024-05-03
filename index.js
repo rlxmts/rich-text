3145062e4e372274508b59f52bcd60110b514b2c
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-05-03 16:06:42 -0300

    adicionando textos e estilos

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-const canva = document.getElementById('campo-texto');
-
-canva.addEventListener( 'select', ()=>{
-    const selectedText = canva.value.substring(canva.selectionStart, canva.selectionEnd);
-    
+const textarea = document.getElementById('campo-texto');
+const botao = document.querySelector('.gerar');
+const listaDeBotoes = document.querySelectorAll('.botoes-personalizar');
+const botaoCopiar = document.querySelector('.copiar');
+const lista = document.querySelector('.lista-texto-formatado');
+//EVENTO PARA COPIAR O TEXTO DENTRO DO TEXTAREA
+botaoCopiar.addEventListener('click', ()=>{
+    textarea.select();
+    try{
+        navigator.clipboard.writeText(textarea.value)
+    }catch(erro){
+        alert(`Texto não copiado. ${erro}`);
+    }
 })
+
+
+listaDeBotoes.forEach( (botao)=> {    
+    botao.onclick = () =>{
+        try{
+            criarItem(botao.classList[1]);
+            textarea.value = '';
+
+        }catch(error){
+            alert(error);
+        }
+    }
+})
+
+
+
+function criarItem(classe){
+    const textoSelecionado = textarea.value;
+
+    if(textoSelecionado == ''){
+        throw new Error("Campo de texto vazio!");
+    }
+    const li = document.createElement('li');
+    li.classList.add(`${classe}`);
+    li.innerText = `${textoSelecionado}`;
+    lista.appendChild(li);
+}
